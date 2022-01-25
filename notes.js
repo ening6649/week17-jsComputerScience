d19622248f0c9513228ab61597e2734566c3deea
ening <ening6649@yahoo.com>
2022-01-25 13:54:08 -0500

    added more notes

--- notes.js
+++ notes.js
@@ -145,3 +145,132 @@ const robot = {
   };
   
   buttonEl.addEventListener('click', clickHandler1);
+
+//example of using data-* to track clicks
+<button id="my-button" data-count="0">Clicks: 0</button>
+const buttonEl = document.getElementById('my-button');
+
+const clickHandler = function() {
+  let count = parseInt(this.getAttribute('data-count'));
+  count++;
+
+  this.setAttribute('data-count', count);
+  this.textContent = `Clicks: ${count}`;
+};
+
+buttonEl.addEventListener('click', clickHandler);
+
+// example to do the same using a closure
+const buttonEl = document.getElementById('my-button');
+
+const clickHandler = function() {
+  let count = 0;
+
+  return function() {
+    count++;
+    this.textContent = `Clicks: ${count}`;
+  };
+};
+
+buttonEl.addEventListener('click', clickHandler());
+
+// if there are more buttons to run, attach the same self contained closure to every
+// ..button element
+const buttons = document.getElementsByTagName('button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', clickHandler());
+}
+
+// example of the same with event delegation
+<div id="container">
+  <button>Button #1</button>
+  <button>Button #2</button>
+  <button>Button #3</button>
+  <button>Button #4</button>
+  <button>Button #5</button>
+</div>
+
+
+const containerEl = document.getElementById('container');
+
+const clickHandler = function(event) {
+  if (event.target.matches('button')) {
+    event.target.textContent = 'Clicked!';
+  }
+};
+
+containerEl.addEventListener('click', clickHandler);
+
+// find a position in an index
+findIndex(23) 
+const findIndex = (num) => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === num) {
+      console.log(`${num} found at index ${i}`);
+    }
+  }
+};
+
+// binary search example
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  if (num === arr[middle]) {
+    return 'found it';
+  }
+  else if (num < arr[middle]) {
+    right = middle;
+    return `bring right down to ${right}`;
+  }
+  else if (num > arr[middle]) {
+    left = middle + 1;
+    return `bring left up to ${left}`;
+  }
+};
+
+console.log(binarySearch(data, 38));
+
+// example of a recursion for the above
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num, left, right) => {
+  let middle = Math.floor((left + right) / 2);
+
+  // range overlapped, so never found number
+  if (left > right) {
+    return -1;
+  }
+  else if (num === arr[middle]) {
+    return middle;
+  }
+  else if (num < arr[middle]) {
+    // call again with a new right value
+    return binarySearch(arr, num, left, middle - 1);
+  }
+  else {
+    // call again with a new left value
+    return binarySearch(arr, num, middle + 1, right);
+  }
+};
+
+// set initial left and right values on first call
+console.log(binarySearch(data, 12, 0, data.length - 1));
+
+// example of a recursion
+const askForFriend = () => {
+  inquirer.prompt({
+    type: 'input',
+    message: 'Enter a name:',
+    name: 'friend'
+  })
+  .then(({ friend }) => {
+    console.log(`Hello, ${friend}!`);
+    askForFriend();
+  });
+ };
+
