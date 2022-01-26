09f659fc39f569bacc3b83ba91f9afc19524d6bb
ening <ening6649@yahoo.com>
2022-01-26 13:19:49 -0500

    added benchmark

--- notes.js
+++ notes.js
@@ -274,3 +274,126 @@ const askForFriend = () => {
   });
  };
 
+//  recursive 
+const loop = (num) => {
+  return loop(num + 1);
+};
+
+console.log(loop(1));
+
+// code a bubble sort , sort an array number to go from lowest to highest 
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      // compare arr[j] to arr[j+1]
+      // swap places if needed
+    }
+  }
+
+  return arr;
+};
+
+// stop running once sorted
+const numbers = [54, 8, 87, 88, 100, 98, 17, 72, 67, 42, 40, 34, 55, 18, 33, 94, 59, 68, 92, 2];
+const bubbleSort = (arr) => {
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+   // compare current element to next
+   if (arr[i] > arr[i+1]) {
+    // swap using a third temp variable
+    let temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+
+    // flag as not sorted to run loop again
+    sorted = false;
+  }
+}
+}
+
+  return arr;
+};
+
+// running bubblesort benchmark
+const Benchmark = require('benchmark');
+const { bubbleSort } = require('./sort');
+
+const numbers = [];
+for (let i = 0; i < 10000; i++) {
+  numbers.push(Math.floor(Math.random() * 10000) + 1);
+}
+
+const suite = new Benchmark.Suite;
+
+suite
+  .add('bubble sort', function() {
+    const testArray = [...numbers];
+    bubbleSort(testArray);
+  })
+  .on('complete', function() {
+    this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
+  })
+  .run();
+
+// example of quick sort 
+const quickSort = (arr) => {
+  // use first index as the pivot point
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  // start at index 1 to ignore pivot
+  for (let i = 1; i < arr.length; i++) {
+    // push into different arrays based on value compared to pivot
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    }
+    else {
+      right.push(arr[i]);
+    }
+  }
+
+  // merge arrays and pivot together
+  return left.concat(pivot, right);
+};
+
+// export along with bubble sort
+module.exports = { bubbleSort, quickSort };
+
+// in a seperate js file
+const { quickSort } = require('./sort');
+const numbers = [54, 8, 87, 88, 100, 98, 17, 72, 67, 42, 40, 34, 55, 18, 33, 94, 59, 68, 92, 2];
+
+console.log(quickSort(numbers));
+
+// another example of quicksort
+const quickSort = (arr) => {
+  // don't quick-sort a small array; just return it immediately
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  // use first index as the pivot point
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  // start at index 1 to ignore pivot
+  for (let i = 1; i < arr.length; i++) {
+    // push into different arrays based on value compared to pivot
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    }
+    else {
+      right.push(arr[i]);
+    }
+  }
+
+  // merge the sorted arrays and pivot together
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
